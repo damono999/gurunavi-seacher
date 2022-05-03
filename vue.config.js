56ab6d595293f0eb9d8f7a7a996ce27dd24909f7
damono99 <kancan877@gmail.com>
2022-05-03 13:55:54 +0900

    set build file to docs directory

--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,40 @@
-const path = require('path')
-const resolve = dir => path.join(__dirname, dir)
+const path = require('path');
+const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = {
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
   },
   css: {
     loaderOptions: {
       scss: {
         data: '@import "@global/global.scss";',
-      }
-    }
+      },
+    },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.alias
-      .set("@atoms", resolve('src/js/components/atoms'))
-      .set("@globals", resolve('src/js/components/globals'))
-      .set("@molecules", resolve('src/js/components/molecules'))
-      .set("@pages", resolve('src/js/pages'))
-      .set("@global", resolve('src/assets/scss/global'))
-      .set("@styles", resolve('src/assets/scss/styles/'))
-      .set("@img", resolve('src/assets/img'))
-      .set("@data", resolve('src/js/data'))
-      .set("@helpers", resolve('src/js/_helpers'))
-      .set("@router", resolve('src/js/_router'))
-      .set("@store", resolve('src/js/_store'));
+      .set('@atoms', resolve('src/js/components/atoms'))
+      .set('@globals', resolve('src/js/components/globals'))
+      .set('@molecules', resolve('src/js/components/molecules'))
+      .set('@pages', resolve('src/js/pages'))
+      .set('@global', resolve('src/assets/scss/global'))
+      .set('@styles', resolve('src/assets/scss/styles/'))
+      .set('@img', resolve('src/assets/img'))
+      .set('@data', resolve('src/js/data'))
+      .set('@helpers', resolve('src/js/_helpers'))
+      .set('@router', resolve('src/js/_router'))
+      .set('@store', resolve('src/js/_store'));
 
     config
       .plugin('web-component-options')
-      .use(
-        require('webpack/lib/DefinePlugin'),
-        [{
-          'API_BASE_URL': 'https://api.gnavi.co.jp/RestSearchAPI/v3/',
-        }]
-      );
+      .use(require('webpack/lib/DefinePlugin'), [
+        {
+          API_BASE_URL: 'https://api.gnavi.co.jp/RestSearchAPI/v3/',
+        },
+      ]);
   },
-}
+};
