59c09f376c4ecca55c5d3cfd2a771fad30d5ff38
damono99 <kancan877@gmail.com>
2022-05-05 10:18:55 +0900

    👍 remove api key

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
 import '@styles/style.scss';
 
-import '@babel/polyfill'
-import Vue from 'vue'
-import App from '@pages'
-import router from '@router'
-import store from '@store'
-import mixins from '@helpers/mixins.js'
-import * as VueGoogleMaps from 'vue2-google-maps'
-import VueLodash from 'vue-lodash'
-
+import '@babel/polyfill';
+import Vue from 'vue';
+import App from '@pages';
+import router from '@router';
+import store from '@store';
+import mixins from '@helpers/mixins.js';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import VueLodash from 'vue-lodash';
 
 Vue.config.productionTip = false;
 Vue.mixin(mixins);
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAJLIXxYzH19mffIJgG8kzsWJILotK5spI',
-    libraries: 'places'
-  }
+    key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+    libraries: 'places',
+  },
 });
-const options = { name: 'lodash' } // customize the way you want to call it
-Vue.use(VueLodash, options) // options is optional
+const options = { name: 'lodash' }; // customize the way you want to call it
+Vue.use(VueLodash, options); // options is optional
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
